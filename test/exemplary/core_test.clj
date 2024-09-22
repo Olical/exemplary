b18253039d6e0662c3c5beb61d3d1eee34a28169
Oliver Caldwell <ollie@oli.me.uk>
2024-09-22 11:24:48 +0100

    Tests are now good and work with watch

--- test/exemplary/core_test.clj
+++ test/exemplary/core_test.clj
@@ -3,7 +3,7 @@
             [exemplary.core :as exemplary]))
 
 (defn square
-  "It squares numbers. The examples here are okay."
+  "It squares numbers."
   {::exemplary/examples
    '((= 100 (square 10))
      (= 25 (square 5)))}
@@ -11,24 +11,18 @@
   (* n n))
 
 (defn half
-  "It halves numbers, this one is broken deliberately."
+  "It halves numbers."
   {::exemplary/example
    '(= 5 (half 10))}
   [n]
-  (* n 0.51))
+  (/ n 2))
 
 (t/deftest var->test-name-symbol
   (t/testing "adds the correct suffix to a vars name and returns it as a symbol"
     (t/is (= 'square-exemplary-test (exemplary/var->test-name-symbol #'square)))))
 
 (defn test-var-fixture
-  "Instrument the test fns for use in meta testing.
-
-  Warning! These tests do not work when re-run inside the same namespace multiple times. So kaocha --watch and running tests in the REPL repeatedly will throw errors.
-
-  This is because we need to undefine the test vars created by the library (which intentionally fail!) before the next run which I can't get working in this fixture for some reason.
-
-  Maybe we should just have all tests pass and just check that they run with the counts, then we can leave them defined... that might be better!"
+  "Instrument the test fns for use in meta testing."
   [f]
   (exemplary/process-var! #'square)
   (exemplary/process-var! #'half)
@@ -38,10 +32,10 @@
 
 (t/deftest process-var!
   (t/testing "embeds their examples into their doc strings"
-    (t/is (= "It squares numbers. The examples here are okay.\n\nExamples:\n (= 100 (square 10))\n (= 25 (square 5))"
+    (t/is (= "It squares numbers.\n\nExamples:\n (= 100 (square 10))\n (= 25 (square 5))"
              (:doc (meta #'square))))
 
-    (t/is (= "It halves numbers, this one is broken deliberately.\n\nExamples:\n (= 5 (half 10))"
+    (t/is (= "It halves numbers.\n\nExamples:\n (= 5 (half 10))"
              (:doc (meta #'half)))))
 
   (t/testing "creates test vars under known names"
@@ -49,5 +43,7 @@
     (t/is (var? (resolve 'exemplary.core-test/half-exemplary-test))))
 
   (t/testing "we can execute these tests and check the function"
-    (t/is (t/successful? (t/run-test-var (resolve 'exemplary.core-test/square-exemplary-test))))
-    (t/is (not (t/successful? (t/run-test-var (resolve 'exemplary.core-test/half-exemplary-test)))))))
+    (t/is (= {:test 1, :pass 2, :fail 0, :error 0, :type :summary}
+             (t/run-test-var (resolve 'exemplary.core-test/square-exemplary-test))))
+    (t/is (= {:test 1, :pass 1, :fail 0, :error 0, :type :summary}
+             (t/run-test-var (resolve 'exemplary.core-test/half-exemplary-test))))))
