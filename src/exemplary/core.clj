e8d42fbc867aa8657e2bee7add1f13778ed04495
Oliver Caldwell <ollie@oli.me.uk>
2024-09-22 12:55:38 +0100

    Improve var moving, process all ns for now, but should change this before publish

--- src/exemplary/core.clj
+++ src/exemplary/core.clj
@@ -8,6 +8,26 @@
   [var]
   (symbol (str (:name (meta var)) "-exemplary-test")))
 
+(defn test-ns?
+  "Given a namespace, returns true if it ends in -test."
+  [ns]
+  (str/ends-with? (str ns) "-test"))
+
+(defn ns->test-ns
+  "Given a namespace, returns the -test suffixed version of the same namespace."
+  [ns]
+  (create-ns (symbol (str ns "-test"))))
+
+(defn move-var
+  "Moves a var from one ns to another. Also copies all metadata over to the new var but without the :ns key which will be different now."
+  [from-ns to-ns var-name]
+  (let [var (ns-resolve from-ns var-name)]
+    (intern to-ns var-name var)
+    (ns-unmap from-ns var-name)
+    (let [moved-var (ns-resolve to-ns var-name)]
+      (alter-meta! moved-var merge (dissoc (meta var) :ns))
+      moved-var)))
+
 (defn process-var!
   "The vars examples will be added to the docstring and clojure.test/deftest calls will be created for you within the current namespace. You can use :exemplary.core/examples with a list/vector of examples or :exemplary.core/example with a single example.
 
@@ -51,14 +71,35 @@
                     (str/join "\n"))))))))
 
       (require 'clojure.test)
-      (binding [*ns* (:ns meta)]
-        (eval
-         `(clojure.test/deftest
-            ^:exemplary.core/test
-            ~(var->test-name-symbol var)
-            ~@(map
-               (fn [example]
-                 `(clojure.test/is ~example))
-               examples)))))))
+      (let [original-ns (:ns meta)
+            test-var-name (var->test-name-symbol var)
+            test-var
+            (binding [*ns* original-ns]
+              (eval
+               `(clojure.test/deftest
+                  ^:exemplary.core/test
+                  ~test-var-name
+                  ~@(map
+                     (fn [example]
+                       `(clojure.test/is ~example))
+                     examples))))]
+        (if (test-ns? original-ns)
+          test-var
+          (move-var original-ns (ns->test-ns original-ns) test-var-name))))))
 
-(process-var! #'var->test-name-symbol)
+(defn process-ns!
+  "Runs all vars in a ns through process-var! When given no arguments it will process the current ns."
+  ([] (process-ns! *ns*))
+  ([ns]
+   (doseq [var (vals (ns-interns ns))]
+     (when (var? var)
+       (process-var! var)))))
+
+(defn process-every-ns!
+  "Runs all vars in all namespaces through process-var!"
+  []
+  (doseq [ns (all-ns)]
+    (process-ns! ns)))
+
+(time
+ (process-every-ns!))
