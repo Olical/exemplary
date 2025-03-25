7ef95a1edb2dbf3cddc06fed4f77d17121545643
Oliver Caldwell <ollie@oli.me.uk>
2025-03-25 19:15:31 +0000

    Massively simplify the code #4

--- src/exemplary/core.clj
+++ src/exemplary/core.clj
@@ -1,35 +1,11 @@
 (ns exemplary.core
-  (:require [clojure.string :as str]))
-
-(defn ^:no-doc var->test-name-symbol
-  "Takes a var and returns a simple-symbol to be used in a deftest name."
-  {::example '(= 'var->test-name-symbol-exemplary-test
-                 (var->test-name-symbol #'var->test-name-symbol))}
-  [var]
-  (symbol (str (:name (meta var)) "-exemplary-test")))
-
-(defn ^:no-doc test-ns?
-  "Given a namespace, returns true if it ends in -test."
-  [ns]
-  (str/ends-with? (str ns) "-test"))
-
-(defn ^:no-doc ns->test-ns
-  "Given a namespace, returns the -test suffixed version of the same namespace."
-  [ns]
-  (create-ns (symbol (str ns "-test"))))
-
-(defn ^:no-doc move-var
-  "Moves a var from one ns to another. Also copies all metadata over to the new var but without the :ns key which will be different now."
-  [from-ns to-ns var-name]
-  (let [var (ns-resolve from-ns var-name)]
-    (intern to-ns var-name (var-get var))
-    (ns-unmap from-ns var-name)
-    (let [moved-var (ns-resolve to-ns var-name)]
-      (alter-meta! moved-var merge (dissoc (meta var) :ns))
-      moved-var)))
+  (:require [clojure.string :as str]
+            [clojure.test :as t]))
 
 (defn process-var!
-  "The vars examples will be added to the docstring and clojure.test/deftest calls will be created for you within the current namespace. You can use :exemplary.core/examples with a list/vector of examples or :exemplary.core/example with a single example.
+  "The vars examples will be added to the docstring and the var will become executable by clojure.test. You can use :exemplary.core/examples with a list/vector of examples or :exemplary.core/example with a single example.
+
+  When clojure.test/*load-tests* is false the test metadata will be omitted, only the doc string will be updated.
 
   You must quote your code to ensure it doesn't execute immidiately. So you will have something like one of these in your function params which come after the doc string:
 
@@ -53,40 +29,33 @@
   (assert (var? var) (str "var required, got " (type var)))
 
   (let [meta (meta var)
+        original-ns (:ns meta)
         examples (or (seq (::examples meta))
                      (and (::example meta) [(::example meta)]))]
     (when examples
       (alter-meta!
        var
        (fn [meta]
-         (let [meta (dissoc meta ::examples ::example)]
-           (update
-            meta :doc
-            (fn [original]
-              (str
-               original
-               (when-not (empty? original)
-                 "\n\n")
-               "```clojure\n"
-               (str/join "\n" (map pr-str examples))
-               "\n```"))))))
-
-      (require 'clojure.test)
-      (let [original-ns (:ns meta)
-            test-var-name (var->test-name-symbol var)
-            test-var
-            (binding [*ns* original-ns]
-              (eval
-               `(clojure.test/deftest
-                  ^:exemplary.core/test
-                  ~test-var-name
-                  ~@(map
+         (-> (dissoc meta ::examples ::example)
+             (update
+              :doc
+              (fn [original]
+                (str
+                 original
+                 (when-not (empty? original)
+                   "\n\n")
+                 "```clojure\n"
+                 (str/join "\n" (map pr-str examples))
+                 "\n```")))
+             (cond-> t/*load-tests*
+               (assoc
+                :test
+                (fn []
+                  (binding [*ns* original-ns]
+                    (run!
                      (fn [example]
-                       `(clojure.test/is ~example))
-                     examples))))]
-        (if (test-ns? original-ns)
-          test-var
-          (move-var original-ns (ns->test-ns original-ns) test-var-name))))))
+                       (t/is (eval example)))
+                     examples)))))))))))
 
 (defn process-ns!
   "Runs all vars in a ns through process-var! When given no arguments it will process the current ns."
--- test/exemplary/core_test.clj
+++ test/exemplary/core_test.clj
@@ -18,19 +18,12 @@
   [n]
   (/ n 2))
 
-(t/deftest var->test-name-symbol
-  (t/testing "adds the correct suffix to a vars name and returns it as a symbol"
-    (t/is (= 'square-exemplary-test (exemplary/var->test-name-symbol #'square)))))
-
 (defn test-var-fixture
   "Instrument the test fns for use in meta testing."
   [f]
-  (alter-meta! (intern 'exemplary.core-test 'example-var 42) merge {:foo true})
   (exemplary/process-var! #'square)
   (exemplary/process-var! #'half)
-  (f)
-  (create-ns 'exemplary.made-up-namespace)
-  (ns-unmap 'exemplary.made-up-namespace 'example-var))
+  (f))
 
 (t/use-fixtures :once test-var-fixture)
 
@@ -42,41 +35,11 @@
     (t/is (= "It halves numbers.\n\n```clojure\n(= 5 (half 10))\n```"
              (:doc (meta #'half)))))
 
-  (t/testing "creates test vars under known names"
-    (t/is (var? (resolve 'exemplary.core-test/square-exemplary-test)))
-    (t/is (var? (resolve 'exemplary.core-test/half-exemplary-test))))
-
-  (t/testing "we can execute these tests and check the function"
+  (t/testing "we can execute the vars as tests"
     (t/is (= {:test 1, :pass 2, :fail 0, :error 0, :type :summary}
-             (t/run-test-var (resolve 'exemplary.core-test/square-exemplary-test))))
+             (t/run-test-var (resolve 'exemplary.core-test/square))))
     (t/is (= {:test 1, :pass 1, :fail 0, :error 0, :type :summary}
-             (t/run-test-var (resolve 'exemplary.core-test/half-exemplary-test))))))
-
-(t/deftest test-ns?
-  (t/testing "returns true for test namespaces"
-    (t/is (not (exemplary/test-ns? 'exemplary.core)))
-    (t/is (exemplary/test-ns? 'exemplary.core-test))))
-
-(t/deftest ns->test-ns
-  (t/testing "converts a namespace to a test namespace"
-    (t/is (= 'exemplary.core-test
-             (ns-name
-              (exemplary/ns->test-ns
-               (the-ns 'exemplary.core)))))))
-
-(t/deftest move-var
-  (t/testing "moves a var from one namespace to another and returns the new one, carries over metadata"
-    (t/is (nil? (resolve 'exemplary.made-up-namespace/example-var)))
-    (exemplary/move-var
-     (the-ns 'exemplary.core-test)
-     (create-ns 'exemplary.made-up-namespace)
-     'example-var)
-    (let [var (resolve 'exemplary.made-up-namespace/example-var)]
-      (t/is (= 42 (var-get var)))
-      (t/is (= {:foo true
-                :name 'example-var
-                :ns (the-ns 'exemplary.made-up-namespace)}
-               (meta var))))))
+             (t/run-test-var (resolve 'exemplary.core-test/half))))))
 
 (t/deftest process-ns!
   (t/testing "calls process-var! on every var in an ns"
